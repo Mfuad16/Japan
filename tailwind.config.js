03bcc374f4bde6bfd309e2b43933ea34b33fd9fc
Mfuad16 <73110753+Mfuad16@users.noreply.github.com>
2025-09-29 15:26:53 +0900

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./app.tsx"
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'sans': ['Inter', 'sans-serif'],
+      screens: {
+        'xs': '475px',
       },
-      colors: {
-        primary: {
-          500: '#4F46E5',
-          600: '#4338CA',
-        }
-      },
-      backgroundImage: {
-        'gradient-border': 'linear-gradient(90deg, #EC4899, #F59E0B)',
-        'text-gradient': 'linear-gradient(90deg, #4F46E5, #7C3AED)',
-      }
     },
   },
   plugins: [],
